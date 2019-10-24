846a686aaab991726c989a972af33c04f68a4ed8
Cerbera <45389499+Slackertron@users.noreply.github.com>
2019-10-24 23:07:15 +0200

    Add files via upload

---
+++ TcpServerSkat/EchoService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpServerSkat
+{
+    class EchoService
+    {
+        private TcpClient connectionSocket;
+
+        public EchoService(TcpClient connectionSocket)
+        {
+            // TODO: Complete member initialization
+            this.connectionSocket = connectionSocket;
+        }
+        internal void DoIt()
+        {
+            Stream ns = connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            sw.WriteLine("Er det Personbil eller Elbil, som du vil finde afgiften på ?");
+            string message = "Startup";
+            string answer;
+            while (!String.IsNullOrEmpty(message))
+            {
+                message = sr.ReadLine();
+                Console.WriteLine("Client: " + message);
+
+                // Tager parameteret for biltypen.
+                string parameter2 = message;
+                sw.WriteLine("Indsæt Pris:");
+
+                // Tager parameteret for pris på bilen.
+                message = sr.ReadLine();
+                Console.WriteLine("Client: " + message);
+                double parameter1 = Convert.ToDouble(message);
+
+                // Kører metoden fra Skat.Afgift ved navn BilAfgift, metoden tager to parameter: pris og type.
+                //Derefter bliver resultatet fra metoden konverteret til string, for at blive sendt til client.
+                answer = Convert.ToString(Skat.Afgift.BilAfgift(parameter1, parameter2));
+                sw.WriteLine(answer);
+                sw.WriteLine("Er det Personbil eller Elbil, som du vil finde afgiften på? Ellers tryk enter for at afslutte.");
+            }
+            ns.Close();
+            connectionSocket.Close();
+        }
+    }
+}
---
+++ TcpServerSkat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TcpServerSkat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(ip, 6789);
+
+            serverSocket.Start();
+
+            while (true)
+            {
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Server activated now");
+                EchoService service = new EchoService(connectionSocket);
+
+                Task.Factory.StartNew(() => service.DoIt());
+            }
+            serverSocket.Stop();
+        }
+    }
+
+}
